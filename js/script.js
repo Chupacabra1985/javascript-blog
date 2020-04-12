9ad30d8ef5e229b7ec91afc41f6f5c646b81f975
Jan Roztropinski <jan.roztropinski@twino.pl>
2020-04-12 17:22:01 +0200

    first task

---
+++ js/script.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const titleClickHandler = function(event){
+    const clickedElement = this;
+    console.log('Link was clicked!');
+
+    /* remove class 'active' from all article links  */
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for(let activeLink of activeLinks){
+        activeLink.classList.remove('active');
+    }
+
+    /* add class 'active' to the clicked link */
+    clickedElement.classList.add('active');
+
+    /* remove class 'active' from all articles */
+    const activeArticles = document.querySelectorAll('.posts article.active');
+
+    for(let activeArticle of activeArticles){
+        activeArticle.classList.remove('active');
+    }
+
+    /* get 'href' attribute from the clicked link */
+    const articleSelector = clickedElement.getAttribute('href').replace("#","");
+
+    /* find the correct article using the selector (value of 'href' attribute) */
+    const targetArticle = document.getElementById(articleSelector);
+
+    /* add class 'active' to the correct article */
+    targetArticle.classList.add('active');
+
+    event.preventDefault();
+}
+
+const links = document.querySelectorAll('.titles a');
+
+for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+}
