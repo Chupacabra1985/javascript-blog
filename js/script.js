6096b6786cc83447d09a5ba31b6632a3acbd5270
Jan Roztropinski <jan.roztropinski@twino.pl>
2020-04-26 20:01:15 +0200

    add index function for tags and authors

--- js/script.js
+++ js/script.js
@@ -4,7 +4,11 @@ const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles',
   optArticleTagsSelector = '.post-tags .list',
-  authorSelector = '.post-author';
+  authorSelector = '.post-author',
+  optTagsListSelector = '.list.tags',
+  optCloudClassCount = 5,
+  optCloudClassPrefix = 'tag-size-',
+  optAuthorsListSelector = '.list.authors';
 
 const titleClickHandler = function(event){
   const clickedElement = this;
@@ -126,3 +130,76 @@ function addClickListenersToAuthors() {
 }
 
 addClickListenersToAuthors();
+
+function calculateTagsParams(tags) {
+  const params = {max: 0, min: 9999};
+  for (let tag in tags) {
+    params.max = Math.max(tags[tag], params.max);
+    params.min = Math.min(tags[tag], params.min);
+  }
+
+  return params;
+}
+
+function calculateTagClass(count, params) {
+  const normalizedCount = count - params.min;
+  const normalizedMax = params.max - params.min;
+  const percentage = normalizedCount / normalizedMax;
+
+  return optCloudClassPrefix + Math.floor( percentage * (optCloudClassCount - 1) + 1 );
+}
+
+function generateIndexTags() {
+  let allTags = {};
+  let allTagsHTML = '';
+  let tagList = document.querySelector(optTagsListSelector);
+  const articles = document.querySelectorAll(optArticleSelector);
+
+  for (let article of articles) {
+    const tags = article.getAttribute('data-tags');
+    const articleTagsArray = tags.split(' ');
+    articleTagsArray.forEach((tag) => {
+      if (!allTags[tag]) {
+        allTags[tag] = 1;
+      } else {
+        allTags[tag]++;
+      }
+    });
+  }
+
+  const tagsParams = calculateTagsParams(allTags);
+
+  for (let tag in allTags) {
+    allTagsHTML += '<li class="' + calculateTagClass(allTags[tag], tagsParams) + '"><a href="#' + tag + '">'+ tag + '</a></li>';
+  }
+
+  tagList.innerHTML = allTagsHTML;
+}
+
+generateIndexTags();
+
+function generateIndexAuthors() {
+  let allAuthors = {};
+  let allAuthorsHTML = '';
+  let authorList = document.querySelector(optAuthorsListSelector);
+  const articles = document.querySelectorAll(optArticleSelector);
+
+  for (let article of articles) {
+    const author = article.getAttribute('data-author');
+    if (!allAuthors[author]) {
+      allAuthors[author] = 1;
+    } else {
+      allAuthors[author]++;
+    }
+  }
+
+  const authorsParams = calculateTagsParams(allAuthors);
+
+  for (let author in allAuthors) {
+    allAuthorsHTML += '<li class="' + calculateTagClass(allAuthors[author], authorsParams) + '"><a href="#' + author + '">'+ author + '</a></li>';
+  }
+
+  authorList.innerHTML = allAuthorsHTML;
+}
+
+generateIndexAuthors();
